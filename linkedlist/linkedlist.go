10cefd424ef68a1bd9bad88a0c3c40a19b1a7cb9
yang-wei <yangweilim21@gmail.com>
2020-05-31 22:29:20 +0900

    do things right

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -1,8 +1,8 @@
 package linkedlist
 
 import (
-  "strings"
-  "strconv"
+	"strconv"
+	"strings"
 )
 
 type Node struct {
@@ -23,15 +23,15 @@ func Constructor() MyLinkedList {
 
 /** Get the value of the index-th Node in the linked list. If the index is invalid, return -1. */
 func (this *MyLinkedList) Get(index int) int {
-	Node, n := this.Node, 0
-	for Node != nil {
+	n := 0
+	for node := this.Node; node != nil; node = node.Next {
 		if n == index {
-			return Node.Val
+			return node.Val
 		}
-    n++
-		Node = Node.Next
+		n++
 	}
 	return -1
+
 }
 
 /** Add a Node of value val before the first element of the linked list. After the insertion, the new Node will be the first Node of the linked list. */
@@ -41,49 +41,52 @@ func (this *MyLinkedList) AddAtHead(val int) {
 
 /** Append a Node of value val to the last element of the linked list. */
 func (this *MyLinkedList) AddAtTail(val int) {
-	if this.Node == nil {
-		this.AddAtHead(val)
-	} else {
-		this.Node.Next = &Node{val, nil}
+	dh := &Node{0, this.Node}
+	node := dh
+	for node.Next != nil {
+		node = node.Next
 	}
+	node.Next = &Node{val, nil}
+	this.Node = dh.Next
 }
 
 /** Add a Node of value val before the index-th Node in the linked list. If index equals to the length of linked list, the Node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
 func (this *MyLinkedList) AddAtIndex(index int, val int) {
 	dh, n := &Node{0, this.Node}, 0
-	for dh != nil {
+	for node := dh; node != nil; node = node.Next {
 		if n == index {
-			nextTmp := dh.Next
-			dh.Next = &Node{val, nextTmp}
-			return
+			node.Next = &Node{val, node.Next}
+			break
 		}
-    n++
-		dh = dh.Next
+		n++
 	}
+	this.Node = dh.Next
 }
 
 /** Delete the index-th Node in the linked list, if the index is valid. */
 func (this *MyLinkedList) DeleteAtIndex(index int) {
 	dh, n := &Node{0, this.Node}, 0
-	for dh != nil {
+	for node := dh; node != nil; node = node.Next {
 		if n == index {
-			if dh.Next != nil {
-				dh.Next = dh.Next.Next
+			if node.Next != nil {
+				node.Next = node.Next.Next
 			} else {
-				dh.Next = nil
+				node.Next = nil
 			}
+			break
 		}
-    n++
-		dh = dh.Next
+		n++
 	}
+
+	this.Node = dh.Next
 
 }
 
-func (this *MyLinkedList) toString() string{
-  l, node := make([]string, 0), this.Node
-  for node != nil {
-    l = append(l, strconv.Itoa(node.Val)) 
-    node = node.Next
-  }
-  return strings.Join(l, "->")
+func (this *MyLinkedList) toString() string {
+	l, node := make([]string, 0), this.Node
+	for node != nil {
+		l = append(l, strconv.Itoa(node.Val))
+		node = node.Next
+	}
+	return strings.Join(l, "->")
 }
--- linkedlist/linkedlist_test.go
+++ linkedlist/linkedlist_test.go
@@ -20,40 +20,102 @@ func TestAddAtTail(t *testing.T) {
 
 	obj.AddAtTail(1)
 	obj.AddAtTail(2)
-
-	if a, e := obj.toString(), "1->2"; a != e {
-		t.Fatalf("Expected %v but got %v", e, a)
-	}
-}
-
-func TestAddAtIndex(t *testing.T) {
-	obj := MyLinkedList{
-    Node: &Node{1, &Node{3, nil}},
-  }
-  obj.AddAtIndex(1, 2)
+	obj.AddAtTail(3)
 
 	if a, e := obj.toString(), "1->2->3"; a != e {
 		t.Fatalf("Expected %v but got %v", e, a)
 	}
 }
 
+func TestAddAtIndex(t *testing.T) {
+	obj := MyLinkedList{
+		Node: &Node{1, &Node{3, nil}},
+	}
+	obj.AddAtIndex(1, 2)
+
+	if a, e := obj.toString(), "1->2->3"; a != e {
+		t.Fatalf("Expected %v but got %v", e, a)
+	}
+
+	obj.AddAtIndex(0, 0)
+	if a, e := obj.toString(), "0->1->2->3"; a != e {
+		t.Fatalf("Expected %v but got %v", e, a)
+	}
+
+	obj.AddAtIndex(999, 999)
+	if a, e := obj.toString(), "0->1->2->3"; a != e {
+		t.Fatalf("Expected %v but got %v", e, a)
+	}
+}
+
+func TestAddAtIndex2(t *testing.T) {
+
+	obj := new(MyLinkedList)
+	obj.AddAtIndex(0, 10)
+	obj.AddAtIndex(0, 20)
+	obj.AddAtIndex(1, 30)
+
+	if a, e := obj.Get(0), 20; a != e {
+		t.Fatalf("Expected %v but got %v", e, a)
+	}
+
+	if a, e := obj.toString(), "20->30->10"; a != e {
+		t.Fatalf("Expected %v but got %v", e, a)
+	}
+}
+
 func TestDeleteAtIndex(t *testing.T) {
 	obj := MyLinkedList{
-    Node: &Node{1, &Node{2, &Node{3, nil}}},
-  }
-  obj.DeleteAtIndex(1)
-
+		Node: &Node{1, &Node{2, &Node{3, nil}}},
+	}
+	obj.DeleteAtIndex(1)
 	if a, e := obj.toString(), "1->3"; a != e {
+		t.Fatalf("Expected %v but got %v", e, a)
+	}
+}
+
+func TestDeleteAtIndex2(t *testing.T) {
+	obj := MyLinkedList{
+		Node: &Node{1, &Node{2, &Node{3, nil}}},
+	}
+	obj.DeleteAtIndex(5)
+	if a, e := obj.toString(), "1->2->3"; a != e {
+		t.Fatalf("Expected %v but got %v", e, a)
+	}
+	obj.DeleteAtIndex(0)
+	if a, e := obj.toString(), "2->3"; a != e {
+		t.Fatalf("Expected %v but got %v", e, a)
+	}
+	obj.DeleteAtIndex(999)
+	if a, e := obj.toString(), "2->3"; a != e {
 		t.Fatalf("Expected %v but got %v", e, a)
 	}
 }
 
 func TestGet(t *testing.T) {
 	obj := MyLinkedList{
-    Node: &Node{1, &Node{2, &Node{3, nil}}},
-  }
+		Node: &Node{1, &Node{2, &Node{3, nil}}},
+	}
 
 	if a, e := obj.Get(1), 2; a != e {
 		t.Fatalf("Expected %v but got %v", e, a)
 	}
+}
+
+func TestAll(t *testing.T) {
+	obj := new(MyLinkedList)
+
+	obj.AddAtHead(1)
+	obj.AddAtHead(3)
+	obj.AddAtIndex(1, 2)
+	t.Logf("Node: %v", obj.toString())
+	if a, e := obj.Get(1), 2; a != e {
+		t.Fatalf("Expected %v but got %v. (Node: %v)", e, a, obj.toString())
+	}
+	obj.DeleteAtIndex(0)
+	t.Logf("Node: %v", obj.toString())
+	if a, e := obj.Get(0), 2; a != e {
+		t.Fatalf("Expected %v but got %v. (Node: %v)", e, a, obj.toString())
+	}
+
 }
