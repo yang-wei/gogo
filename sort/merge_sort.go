c2cf61321afe742fa433783c3ea2c8c94ec93946
yang-wei <yangweilim21@gmail.com>
2020-07-28 23:30:23 +0900

    merge sort

--- sort/merge_sort.go
+++ sort/merge_sort.go
@@ -1,33 +1,36 @@
 package sort
 
-func mergeSort(l []int) []int {
-  if len(l) <= 1 {
-    return l
-  }
-  half := len(l) / 2
-  left, right := mergeSort(l[:half]), mergeSort(l[half:])
-
-  res := make([]int, 0, len(l))
-  i, j := 0, 0
-  for i < len(left) || j < len(right) {
-      if i == len(left) {
-          res = append(res, right[j:]...)
-          break
-      }
-
-      if j == len(right) {
-          res = append(res, left[i:]...)
-          break
-      }
-
-      if left[i] < right[j] {
-         res = append(res, left[i])
-         i++
-      } else {
-         res = append(res, right[j])
-         j++
-      }
-  }
-  return res
+func mergeSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+	middle := len(arr) / 2
+	left := mergeSort(arr[:middle])
+	right := mergeSort(arr[middle:])
+	return merge(left, right)
 }
 
+func merge(left, right []int) []int {
+
+	res := make([]int, len(left)+len(right))
+	i, j := 0, 0
+	for i < len(left) && j < len(right) {
+
+		if left[i] < right[j] {
+			res[i+j] = left[i]
+			i++
+		} else {
+			res[i+j] = right[j]
+			j++
+		}
+	}
+	for i < len(left) {
+		res[i+j] = left[i]
+		i++
+	}
+	for j < len(right) {
+		res[i+j] = right[j]
+		j++
+	}
+	return res
+}
