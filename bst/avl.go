f5252bd2584bb1361a7034a2cca7cd17f425e26d
yang-wei <yangweilim21@gmail.com>
2020-08-06 00:02:08 +0900

    avl insert

---
+++ bst/avl.go
@@ -0,0 +1,145 @@
+package bst
+
+import (
+	"fmt"
+	"strings"
+)
+
+type AVLNode struct {
+	Val    int
+	Height int
+	Left   *AVLNode
+	Right  *AVLNode
+}
+
+func NewAVLNode(val int) *AVLNode {
+	return &AVLNode{
+		Val:    val,
+		Height: 0,
+		Left:   nil,
+		Right:  nil,
+	}
+}
+
+func insert(root *AVLNode, val int) *AVLNode {
+	if root == nil {
+		return NewAVLNode(val)
+	}
+	if val > root.Val {
+		root.Right = insert(root.Right, val)
+	} else if root.Val > val {
+		root.Left = insert(root.Left, val)
+	} else {
+		return root
+	}
+	updateHeight(root)
+	return balance(root)
+}
+
+func balance(root *AVLNode) *AVLNode {
+	left, right := getHeight(root.Left), getHeight(root.Right)
+	if left-right > 1 {
+		// left right
+		if getHeight(root.Left.Right) > getHeight(root.Left.Left) {
+			root.Left = leftRotate(root.Left)
+		}
+		// left left
+		return rightRotate(root)
+	} else if right-left > 1 {
+		// right left
+		if getHeight(root.Right.Left) > getHeight(root.Right.Right) {
+			root.Right = rightRotate(root.Right)
+		}
+		// right right
+		return leftRotate(root)
+	}
+	return root
+}
+
+func rightRotate(root *AVLNode) *AVLNode {
+	next := root.Left
+	root.Left = next.Right
+	next.Right = root
+	updateHeight(next.Right)
+	updateHeight(next)
+	return next
+}
+
+func leftRotate(root *AVLNode) *AVLNode {
+	next := root.Right
+	root.Right = next.Left
+	next.Left = root
+	updateHeight(next.Left)
+	updateHeight(next)
+	return next
+}
+
+func getHeight(root *AVLNode) int {
+	if root == nil {
+		return -1
+	}
+	return root.Height
+}
+
+func updateHeight(root *AVLNode) {
+	l, r := -1, -1
+	if root.Left != nil {
+		l = root.Left.Height
+	}
+	if root.Right != nil {
+		r = root.Right.Height
+	}
+	if l > r {
+		root.Height = l + 1
+	} else {
+		root.Height = r + 1
+	}
+}
+
+func height(node *AVLNode) int {
+	if node == nil {
+		return 0
+	}
+	return node.Height
+}
+
+func (node *AVLNode) print() string {
+
+	queue := make([]*AVLNode, 0)
+	queue = append(queue, node)
+
+	rows := make([][]string, 0)
+
+	for i := 1; len(queue) > 0; i = 2 * i {
+		row := make([]string, i)
+		ele := queue
+		queue = nil
+		// dequeue
+		for i, e := range ele {
+			if e == nil {
+				row[i] = "x"
+				continue
+			} else {
+				row[i] = fmt.Sprintf("%d(%d)", e.Val, e.Height)
+			}
+			queue = append(queue, e.Left)
+			queue = append(queue, e.Right)
+		}
+		rows = append(rows, row)
+	}
+
+	ss := make([]string, 0)
+	ss = append(ss, "")
+	for _, row := range rows {
+		ss = append(ss, strings.Join(row, " "))
+	}
+	ss = ss[:len(ss)-1]
+
+	// indent row
+	for i := len(ss) - 1; i >= 0; i-- {
+		spaceToIndent := strings.Repeat("  ", len(ss)-i)
+		ss[i] = fmt.Sprintf("%s%s", spaceToIndent, ss[i])
+	}
+
+	return strings.Join(ss, "\n")
+}
---
+++ bst/avl_test.go
@@ -0,0 +1,269 @@
+package bst
+
+import (
+	"reflect"
+	"testing"
+)
+
+func tree() *AVLNode {
+	return &AVLNode{
+		Val:    41,
+		Height: 3,
+		Left: &AVLNode{
+			Val: 20, Height: 2,
+			Left: &AVLNode{
+				Val:    11,
+				Height: 0,
+			},
+			Right: &AVLNode{
+				Val:    29,
+				Height: 1,
+				Left: &AVLNode{
+					Val:    26,
+					Height: 0,
+				},
+			},
+		},
+		Right: &AVLNode{
+			Val:    65,
+			Height: 1,
+			Left: &AVLNode{
+				Val:    50,
+				Height: 0,
+			},
+		},
+	}
+}
+
+func TestInsert1(t *testing.T) {
+	node := &AVLNode{
+		Val:    26,
+		Height: 1,
+		Left: &AVLNode{
+			Val:    23,
+			Height: 0,
+		},
+	}
+	actual := insert(node, 29)
+	expected := &AVLNode{
+		Val:    26,
+		Height: 1,
+		Left: &AVLNode{
+			Val:    23,
+			Height: 0,
+		},
+		Right: &AVLNode{
+			Val:    29,
+			Height: 0,
+		},
+	}
+
+	assert(t, expected, actual)
+}
+
+func TestInsert2(t *testing.T) {
+	node := &AVLNode{
+		Val:    26,
+		Height: 1,
+		Left: &AVLNode{
+			Val:    23,
+			Height: 0,
+		},
+	}
+	actual := insert(node, 23)
+	expected := &AVLNode{
+		Val:    26,
+		Height: 1,
+		Left: &AVLNode{
+			Val:    23,
+			Height: 0,
+		},
+	}
+	assert(t, expected, actual)
+}
+
+// left left case
+func TestInsert3(t *testing.T) {
+	node := &AVLNode{
+		Val:    26,
+		Height: 1,
+		Left: &AVLNode{
+			Val:    23,
+			Height: 0,
+		},
+	}
+	actual := insert(node, 20)
+	expected := &AVLNode{
+		Val:    23,
+		Height: 1,
+		Left: &AVLNode{
+			Val:    20,
+			Height: 0,
+		},
+		Right: &AVLNode{
+			Val:    26,
+			Height: 0,
+		},
+	}
+	assert(t, expected, actual)
+}
+
+// left right case
+func TestInsert4(t *testing.T) {
+	node := &AVLNode{
+		Val:    26,
+		Height: 1,
+		Left: &AVLNode{
+			Val:    23,
+			Height: 0,
+		},
+	}
+	actual := insert(node, 24)
+	expected := &AVLNode{
+		Val:    24,
+		Height: 1,
+		Left: &AVLNode{
+			Val:    23,
+			Height: 0,
+		},
+		Right: &AVLNode{
+			Val:    26,
+			Height: 0,
+		},
+	}
+	assert(t, expected, actual)
+}
+
+// right right
+func TestInsert5(t *testing.T) {
+	node := &AVLNode{
+		Val:    26,
+		Height: 1,
+		Right: &AVLNode{
+			Val:    29,
+			Height: 0,
+		},
+	}
+	actual := insert(node, 32)
+	expected := &AVLNode{
+		Val:    29,
+		Height: 1,
+		Left: &AVLNode{
+			Val:    26,
+			Height: 0,
+		},
+		Right: &AVLNode{
+			Val:    32,
+			Height: 0,
+		},
+	}
+	assert(t, expected, actual)
+}
+
+// right left
+func TestInsert6(t *testing.T) {
+	node := &AVLNode{
+		Val:    26,
+		Height: 1,
+		Right: &AVLNode{
+			Val:    29,
+			Height: 0,
+		},
+	}
+	actual := insert(node, 27)
+	expected := &AVLNode{
+		Val:    27,
+		Height: 1,
+		Left: &AVLNode{
+			Val:    26,
+			Height: 0,
+		},
+		Right: &AVLNode{
+			Val:    29,
+			Height: 0,
+		},
+	}
+	assert(t, expected, actual)
+}
+
+func TestInsert7(t *testing.T) {
+	node := NewAVLNode(5)
+	node = insert(node, 6)
+	node = insert(node, 7)
+
+	expected := &AVLNode{
+		Val:    6,
+		Height: 1,
+		Left: &AVLNode{
+			Val:    5,
+			Height: 0,
+		},
+		Right: &AVLNode{
+			Val:    7,
+			Height: 0,
+		},
+	}
+	assert(t, expected, node)
+
+	node = insert(node, 4)
+	node = insert(node, 3)
+
+	expected = &AVLNode{
+		Val:    6,
+		Height: 2,
+		Left: &AVLNode{
+			Val:    4,
+			Height: 1,
+			Left: &AVLNode{
+				Val:    3,
+				Height: 0,
+			},
+			Right: &AVLNode{
+				Val:    5,
+				Height: 0,
+			},
+		},
+		Right: &AVLNode{
+			Val:    7,
+			Height: 0,
+		},
+	}
+	assert(t, expected, node)
+
+	node = insert(node, 2)
+
+	expected = &AVLNode{
+		Val:    4,
+		Height: 2,
+		Left: &AVLNode{
+			Val:    3,
+			Height: 1,
+			Left: &AVLNode{
+				Val:    2,
+				Height: 0,
+			},
+		},
+		Right: &AVLNode{
+			Val:    6,
+			Height: 1,
+			Left: &AVLNode{
+				Val:    5,
+				Height: 0,
+			},
+			Right: &AVLNode{
+				Val:    7,
+				Height: 0,
+			},
+		},
+	}
+	assert(t, expected, node)
+
+}
+
+func assert(t *testing.T, expected, actual *AVLNode) {
+	if e, a := expected, actual; !reflect.DeepEqual(e, a) {
+		t.Logf("Exepected %s", e.print())
+		t.Logf("Actual %s", a.print())
+		t.Fatalf("Expected %v but got %v", e, a)
+	}
+}
